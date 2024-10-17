cf7cfc366f20bee3da1e51b7283bf9b78902b939
Alexey Kazantsev <kazantsevalant@gmail.com>
2024-10-17 19:06:41 +0300

    replacing csv to DB uploading

--- src/data/ticker_data_extractor.py
+++ src/data/ticker_data_extractor.py
@@ -1,6 +1,24 @@
-import yfinance as yf
+import os
 import pandas as pd
+import yfinance as yf
 from datetime import datetime
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+
+load_dotenv()
+db_config = {
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT'),
+    'database': os.getenv('DB_NAME')
+}
+
+
+db_url = f"mysql+pymysql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
+engine = create_engine(db_url)
+
 
 tickers = {
     "WTI": "CL=F",        # WTI Crude Oil
@@ -9,17 +27,24 @@ tickers = {
     "Nasdaq-100": "QQQ"   # Invesco QQQ Trust (replaces ^NDX)
 }
 
-
 start_date = "2022-01-01"
 end_date = datetime.now().strftime('%Y-%m-%d')
 
-def download_and_save_data(ticker, name):
-    data = yf.download(ticker, start=start_date, end=end_date)
-    file_name = f"{name}_data.csv"
-    data.to_csv(file_name)
-    print(f"Data for {name} saved into {file_name}")
+all_data = pd.DataFrame()
+
+def download_and_append_data(ticker, name):
+    global all_data
+    
+    data = yf.download(ticker, start=start_date, end=end_date)    
+    data['Ticker'] = name
+    
+    all_data = pd.concat([all_data, data])
 
 for name, ticker in tickers.items():
-    download_and_save_data(ticker, name)
+    download_and_append_data(ticker, name)
 
-print("Historical data loaded")
+# uploading
+all_data.to_sql(name='market_data', con=engine, if_exists='replace', index=False)
+
+print("Historical data loaded into DB")
+all_data

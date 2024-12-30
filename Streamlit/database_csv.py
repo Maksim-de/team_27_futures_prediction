85f07d2cf258b5f43bde33cf1b94bcb904b62bad
Kseniia <ksbalitskaya@edu.hse.ru>
2024-12-30 19:27:29 +0300

    change

--- Streamlit/database_csv.py
+++ Streamlit/database_csv.py
@@ -9,7 +9,7 @@ def load_data(file):
 def run(data):
     # Дописать проверку на соответствие со столбцами
     df_csv = load_data(data)
-    required_columns = ["close", "business_date", "ticker"]
+    required_columns = ["close", "business_date", "ticker", "open", "high", "low"]
     if all(column in df_csv.columns for column in required_columns):
         st.session_state.data = df_csv  # Сохраняем данные в session_state
         return 2
